a8cbd93cd8ed1ca5784d6873cec588593e8a7f77
Peter Sagerson <psagers.hg@ignorare.net>
2013-09-10 16:35:25 -0700

    Python 3 compatibility.
    Versions are bumped to 0.2 and the minimum Django requirement is now 1.4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='django-otp-twilio',
-    version='0.1.3',
+    version='0.2.0',
     description="A django-otp plugin that delivers tokens via Twilio's SMS service.",
     long_description=open('README').read(),
     author='Peter Sagerson',
@@ -16,15 +16,21 @@ setup(
     url='https://bitbucket.org/psagers/django-otp',
     license='BSD',
     install_requires=[
-        'django-otp',
+        'django-otp >= 0.2.0',
         'requests',
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Security",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
     ],
 )
