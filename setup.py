071d24f1a28543172728b77908c4ce490cb475fd
Peter Sagerson <psagers@ignorare.net>
2016-01-10 13:53:25 -0800

    Minor releases.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-otp-twilio',
-    version='0.3.3',
+    version='0.3.4',
     description="A django-otp plugin that delivers tokens via Twilio's SMS service.",
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
