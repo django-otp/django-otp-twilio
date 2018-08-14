30d1817c9bc87db9d9a05615fc9a587f92fae41b
Peter Sagerson <psagers@ignorare.net>
2018-08-14 10:28:13 -0700

    New versions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-otp-twilio',
-    version='0.4.2',
+    version='0.5.0',
     description="A django-otp plugin that delivers tokens via Twilio's SMS service.",
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
@@ -14,7 +14,7 @@ setup(
     url='https://bitbucket.org/psagers/django-otp',
     license='BSD',
     install_requires=[
-        'django-otp >= 0.4.0',
+        'django-otp >= 0.5.0',
         'requests',
     ],
     classifiers=[
